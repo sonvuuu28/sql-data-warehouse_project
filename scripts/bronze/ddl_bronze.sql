b39c5537da1d11983b007df826a6810bee172c9e
sonvuuu28 <Vuthienson280104@gmail.com>
2025-09-09 16:29:49 +0700

    Add files via upload

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,12 @@
+use DataWarehouse;
+go
+
+create table bronze.crm_cust_info(
+cst_id int,
+cst_key nvarchar(50),
+cst_firstname nvarchar(50),
+cst_lastname nvarchar(50),
+cst_marital_status nvarchar(50),
+cst_gndr nvarchar(50),
+cst_create_date date,
+);
